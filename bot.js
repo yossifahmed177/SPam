c8f8a801eaaf99efcf1a94cc989e33f0cdaefe6a
yossifahmed177 <44752698+yossifahmed177@users.noreply.github.com>
2018-11-21 04:57:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,12 +97,3 @@ if (message.content === 'spam') {
 
 client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
-client3.login(process.env.TOKEN3);
-client4.login(process.env.TOKEN4);
-client5.login(process.env.TOKEN5);
-client6.login(process.env.TOKEN6);
-client7.login(process.env.TOKEN7);
-client8.login(process.env.TOKEN8);
-client9.login(process.env.TOKEN9);
-client10.login(process.env.TOKEN10);
-
