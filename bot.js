0b6cb64a13b6afa301127cb1983bc97ddfe85fdf
yossifahmed177 <44752698+yossifahmed177@users.noreply.github.com>
2018-11-21 04:44:18 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,422 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : YossiF `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+
+client.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+client2.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client2.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client3.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client3.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+
+client4.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client4.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client4.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client4.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+
+client5.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client5.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+
+
+client6.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client6.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+
+client7.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client7.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+client8.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client8.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client8.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client9.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client9.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client9.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+client10.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client10.on('message', message => {
+    if(message.content === '3'){
+        message.channel.send('#rep <@408396389291393025>')
+    }
+});
+
+
+
+
+
+client10.on('message', message => { 
+if (message.content === 'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit  , Farm Credit , ez Farm - سبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7);
+client8.login(process.env.TOKEN8);
+client9.login(process.env.TOKEN9);
+client10.login(process.env.TOKEN10);
+
